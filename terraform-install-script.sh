0672c79e310923952b6f7a7b90dfc7661b577c0c
AqibHAfeez <aqibhafeez473@gmail.com>
2024-01-30 10:11:36 +0000

    terraform script

---
+++ terraform-install-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script to install HashiCorp Terraform
+
+# Download and add HashiCorp GPG key to keyring
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+# Add HashiCorp repository to sources.list.d
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Update package list
+sudo apt update
+
+# Install Terraform
+sudo apt install terraform
+
